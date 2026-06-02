ff48c021aa86f1bdf3d6ae1c6e4fa42942645091
m7mdaljml <124713884+m7mdaljml@users.noreply.github.com>
2026-06-02 19:18:50 +0300

    fix: update proxy target

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://react-projects.freedev.app",
+        target: "", // Should add here the production url
         changeOrigin: true,
         secure: true,
       },
